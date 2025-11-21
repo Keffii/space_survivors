440f683a14493a8e261b3cf466f9f56dc3f22fde
Kevin <kevinbogosta.vallin@yh.nackademin.se>
2025-11-21 12:11:13 +0100

    Add Device management UI with create, list, and delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,114 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { useAuthenticator } from '@aws-amplify/ui-react';
+
+import {
+  Badge,
+  Button,
+  Card,
+  Collection,
+  Divider,
+  Flex,
+  Heading,
+  useAuthenticator,
+  View
+} from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>();
 
-function App() {
-  const { user, signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
+function App() {
+
+  const { user, signOut } = useAuthenticator();
+
+  const [devices, setDevices] = useState<Array<Schema["Device"]["type"]>>([]);
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
+    client.models.Device.observeQuery().subscribe({
+      next: (data) => { setDevices([...data.items]) },
     });
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+  function createDevice() {
+    const device = String(window.prompt("Device ID"));
+    client.models.Device.create({ device_id: device, owner: user.userId })
   }
 
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
+  function deleteDevice(device_id: string) {
+    client.models.Device.delete({ device_id })
   }
+
+
+  //const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  //useEffect(() => {
+  // client.models.Todo.observeQuery().subscribe({
+  //   next: (data) => setTodos([...data.items]),
+  // });
+  //}, []);
+
+  // function createTodo() {
+  //client.models.Todo.create({ content: window.prompt("Todo content") });
+  //}
+
+
+  //function deleteTodo(id: string) {
+  //client.models.Todo.delete({ id })
+  // }
 
   return (
     <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <h1>My todos</h1>
-      <button onClick={signOut}>Sign out</button>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map(todo => <li
+      <h1>{user?.signInDetails?.loginId}'s Devices</h1>
+      <Divider padding="xs" />
+      <h3>Devices</h3>
+      {
+        <Button
+          variation="primary"
+          loadingText=""
+          onClick={createDevice}
+        >
+          Add Device
+        </Button>
+      }
+      <Divider padding="xs" />
 
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>
-          {todo.content}
-        </li>)}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/">Review next step of this tutorial.</a>
-      </div>
+      <Collection
+        items={devices}
+        type="list"
+        direction="row"
+        gap="20px"
+        wrap="nowrap"
+      >
+        {(item, index) => (
+          <Card
+            key={index}
+            borderRadius="medium"
+            maxWidth="20rem"
+            variation="outlined"
+          >
+            <View padding="xs">
+              <Flex>
+                {/* Last Seen: {telemetries[telemetries.length - 1]?.timestamp ? moment(telemetries[telemetries.length - 1].timestamp).fromNow() : ""} */}
+
+              </Flex>
+              <Flex>
+                Status:
+                <Badge variation={(item?.status == "connected") ? "success" : "error"} key={item.device_id}>
+                  {item?.status ? item?.status.charAt(0).toUpperCase() + String(item?.status).slice(1) : ""}
+                </Badge>
+              </Flex>
+              <Divider padding="xs" />
+              <Heading padding="medium">ID: {item.device_id}</Heading>
+              <Button variation="destructive" isFullWidth onClick={() => deleteDevice(item.device_id)}>
+                Delete
+              </Button>
+            </View>
+          </Card>
+        )}
+      </Collection>
+      <View padding="xs"></View>
+      <button onClick={signOut}>Sign out</button>
     </main>
-  )
+  );
 }
 
 export default App;
