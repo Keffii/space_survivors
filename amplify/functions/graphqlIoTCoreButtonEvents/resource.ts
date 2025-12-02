242cdbf77d8ea0c3b26180ec01e6f9a7dc712bb4
Kevin <kevinbogosta.vallin@yh.nackademin.se>
2025-12-02 08:28:54 +0100

    fix: install amplify dependencies in CI/CD pipeline

--- amplify/functions/graphqlIoTCoreButtonEvents/resource.ts
+++ amplify/functions/graphqlIoTCoreButtonEvents/resource.ts
@@ -8,6 +8,6 @@ export const graphqlIoTCoreButtonEvents = defineFunction({
   },
   // optionally specify a name for the Function (defaults to directory name)
   name: 'graphqlIoTCoreButtonEvents',
-  // optionally specify a path to your handler, (defaults to "./handler.ts")
+  // optionally specify a path to your handler (defaults to "./handler.ts")
   entry: './handler.ts'
 });
